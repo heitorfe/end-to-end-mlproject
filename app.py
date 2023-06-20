f940ac54a8f2e4cff911ca4ef8101076b255736b
heitorfe <heitorfelix@yahoo.com.br>
2023-06-20 06:42:06 -0300

    app deploy

--- app.py
+++ app.py
@@ -2,11 +2,8 @@ from flask import Flask, request, render_template
 import numpy as np
 import pandas as pd
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
-from sklearn.preprocessing import StandardScaler
-
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
